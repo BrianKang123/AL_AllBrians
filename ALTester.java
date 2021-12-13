50b7653cd8de3cbaab37de607ff29d6dd9ecf28e
unknown <bwang30@stuy.edu>
2021-12-12 21:11:58 -0500

    added functionality to OrderedArrayList

--- ALTester.java
+++ ALTester.java
@@ -2,14 +2,25 @@ import java.util.ArrayList;
 public class ALTester{
 
   public static void main(String[] args){
-    ArrayList<Integer> test = new ArrayList<Integer>();
+    ArrayList<Integer> unorder = new ArrayList<Integer>();
     for(int i = 0 ; i < 23 ; i += 1){
-      //test.add((int)(Math.random() * 10));
-      test.add(i);
+      unorder.add((int)(Math.random() * 10));
+      //unorder.add(i);
     }
-    System.out.println(test.toString());
-    System.out.println(isSorted(test));
+    System.out.println(unorder.toString());
+    System.out.println(isSorted(unorder));
+
+    OrderedArrayList order = new OrderedArrayList();
+    for(int i = 0 ; i < 23 ; i += 1){
+      order.add(unorder.get(i));
+      //order.add(i);
+    }
+    System.out.println(order.toString());
+    //isSorted is not made to handle OrderedArrayLists, but they should be in order regardless
+    //System.out.println(isSorted(order));
+
   }
+
   public static boolean isSorted(ArrayList<Integer> al){
     int prev = al.get(0);
     for(int i = 1 ; i < al.size() ; i += 1){
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,34 +1,48 @@
 import java.util.ArrayList;
 
 public class OrderedArrayList {
-  private ArrayList _data;
+  private ArrayList<Integer> _data;
+
+  public OrderedArrayList(){
+    _data = new ArrayList<Integer>();
+  }
 
   public boolean add(int newVal) {
     int idx = calcIndex(newVal);
-
-    
+    _data.add(idx, newVal);
+    return true;
   }
 
-  public int remove() {
-
-  }
-
-  public int remove(int index) {
-
+  public int remove(int idx) {
+    int temp = _data.get(idx);
+    _data.remove(idx);
+    return temp;
   }
 
   public int size() {
-    return _data.size()
+    return _data.size();
   }
 
   private int calcIndex(int newVal) {
     for (int i = 0; i < _data.size(); i += 1) {
-      if (_data.get(i) < newVal) {
+      if (_data.get(i) > newVal) {
         return i;
       }
     }
 
     return size();
+  }
+
+  public String toString(){
+    String result = "[";
+    for(int i = 0 ; i < _data.size() ; i += 1){
+      result += _data.get(i) + ", ";
+    }
+    if ( result.length() > 1 )
+            //shave off trailing comma
+            result = result.substring( 0, result.length() - 2 );
+    result += "]";
+    return result;
   }
 
 }
